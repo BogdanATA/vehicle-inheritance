642881c6750ebf3a7c360037601e434dbfb148f3
BogdanATA <batameev@gmail.com>
2026-05-11 13:47:03 -0400

    created instance of each of the vehicle types and set some values for them

---
+++ src/main/java/com/pluralsight/VehicleInheritance.java
@@ -0,0 +1,28 @@
+package com.pluralsight;
+
+public class VehicleInheritance {
+    public static void main(String[] args) {
+        Moped slowRide = new Moped();
+        slowRide.setTopSpeed(30);
+        slowRide.setFuelCapacity(6);
+        slowRide.setNumberOfPassengers(2);
+
+        Car dailyDriver = new Car();
+        dailyDriver.setColor("Pink");
+        dailyDriver.setNumberOfDoors(4);
+        dailyDriver.setTopSpeed(210);
+
+        SemiTruck peterbilt = new SemiTruck();
+        peterbilt.setCargoCapacity(80000);
+        peterbilt.setNumberOfPassengers(2);
+        peterbilt.setNumberOfTrailers(1);
+
+        Hovercraft hover = new Hovercraft();
+        hover.setColor("Orange");
+        hover.setTopSpeed(35);
+        hover.setNumberOfPassengers(1);
+
+
+
+    }
+}
